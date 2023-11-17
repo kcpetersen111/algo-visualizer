f18f5267c415f5c73a8b4260527b68ff39fac588
maddie <rasmussen.madalynn@gmail.com>
2023-11-17 14:14:28 -0700

    q

--- src/app/backend/algorithms/bfs.cpp
+++ src/app/backend/algorithms/bfs.cpp
@@ -1,76 +1,54 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
- 
-#define MAX_VERTICES 50
- 
-// This struct represents a directed graph using
-// adjacency list representation
-typedef struct Graph_t {
- 
-    // No. of vertices
-    int V;
-    bool adj[MAX_VERTICES][MAX_VERTICES];
-} Graph;
- 
-// Constructor
-Graph* Graph_create(int V)
-{
-    Graph* g = malloc(sizeof(Graph));
-    g->V = V;
- 
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
-            g->adj[i][j] = false;
-        }
-    }
- 
-    return g;
-}
- 
-// Destructor
-void Graph_destroy(Graph* g) { free(g); }
- 
-// Function to add an edge to graph
-void Graph_addEdge(Graph* g, int v, int w)
-{
-    // Add w to v’s list.
-    g->adj[v][w] = true;
-}
- 
-// Prints BFS traversal from a given source s
-void Graph_BFS(Graph* g, int s)
-{
-    // Mark all the vertices as not visited
-    bool visited[MAX_VERTICES];
-    for (int i = 0; i < g->V; i++) {
-        visited[i] = false;
-    }
- 
-    // Create a queue for BFS
-    int queue[MAX_VERTICES];
-    int front = 0, rear = 0;
- 
-    // Mark the current node as visited and enqueue it
-    visited[s] = true;
-    queue[rear++] = s;
- 
-    while (front != rear) {
- 
-        // Dequeue a vertex from queue and print it
-        s = queue[front++];
-        printf("%d ", s);
- 
-        // Get all adjacent vertices of the dequeued
-        // vertex s.
-        // If an adjacent has not been visited,
-        // then mark it visited and enqueue it
-        for (int adjacent = 0; adjacent < g->V;
-             adjacent++) {
-            if (g->adj[s][adjacent] && !visited[adjacent]) {
-                visited[adjacent] = true;
-                queue[rear++] = adjacent;
+#include "../graph.h"
+#include <queue>
+#include <iostream>
+
+class BFSGraph : public Graph {
+public:
+    BFSGraph(int size, int startNode, int endNode) : Graph(size, startNode, endNode) {}
+    void NextStep() override;
+    void BFS();
+};
+
+void BFSGraph::BFS() {
+    std::queue<int> q;
+    std::vector<bool> visited(mGraph.size(), false);
+
+    q.push(mStart);
+    visited[mStart] = true;
+
+    while (!q.empty()) {
+        int currentNode = q.front();
+        std::cout << "Visiting Node: " << currentNode << std::endl;
+        q.pop();
+
+        for (int i = 0; i < mGraph[currentNode].size(); ++i) {
+            if (mGraph[currentNode][i] != 0 && !visited[i]) {
+                q.push(i);
+                visited[i] = true;
             }
         }
     }
+}
+
+void BFSGraph::NextStep() {
+    BFS();
+}
+
+int main() {
+    // Example usage
+    int size = 5;
+    int startNode = 0;
+    int endNode = 4;
+
+    BFSGraph bfsGraph(size, startNode, endNode);
+
+    bfsGraph.AddConnection(0, 1, 1);
+    bfsGraph.AddConnection(0, 2, 1);
+    bfsGraph.AddConnection(1, 3, 1);
+    bfsGraph.AddConnection(2, 4, 1);
+
+    std::cout << "BFS traversal:" << std::endl;
+    bfsGraph.NextStep();
+
+    return 0;
 }
