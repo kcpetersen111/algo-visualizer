e2d4f6f505cc1902f5352cbf443f938797b8f577
bashton <14bashton@gmail.com>
2023-11-17 14:33:58 -0700

    toolbar start

--- src/app/components/ToolBar.tsx
+++ src/app/components/ToolBar.tsx
@@ -1,15 +1,19 @@
-import { IconPlus } from "@tabler/icons-react";
+import { IconPlus, IconX } from "@tabler/icons-react";
 
 type ToolBarProps = {
     addNode: Function;
+    removeNode: Function;
 }
 
-export const ToolBar = ({ addNode }: ToolBarProps) => {
+export const ToolBar = ({ addNode, removeNode }: ToolBarProps) => {
     return (
         <>
             <div className="flex flex-col h-full w-72 border-2">
-                <button onClick={() => addNode("test")}>
+                <button onClick={() => addNode()}>
                     <IconPlus />
+                </button>
+                <button onClick={() => {removeNode(); console.log('remove')}}>
+                    <IconX />
                 </button>
             </div>
         </>
