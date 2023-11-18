ec037b00bf7431f9e253f8f1b833260afc95400b
bashton <14bashton@gmail.com>
2023-11-17 23:56:50 -0700

    popover

--- src/app/components/ToolBar.tsx
+++ src/app/components/ToolBar.tsx
@@ -14,6 +14,19 @@ type ToolBarProps = {
 
 const RunAlgorithmPopover = () => {
     // Element
+
+    return (
+        <>
+            <div className="flex flex-col w-fit h-fit absolute right-20 border-2 rounded-md p-4">
+                <p>Choose an Algorithm to run:</p>
+                <select name="yup" className="w-fit">
+                    <option>Breadth First Search</option>
+                    <option>Depth First Search</option>
+                </select>
+                <button className="mt-8 bg-blue-400 text-white rounded-md">Submit</button>
+            </div>
+        </>
+    );
 }
 
 const SettingsPopover = () => {
@@ -30,31 +43,34 @@ export const ToolBar = ({ activateAdd, removeNode, activateConnect, selectNode, 
     const buttonStyles = "hover:bg-slate-200 rounded-lg p-2 my-2 "
 
     const nodeRef = useRef(null);
+    
+    const [playPopover, setPlayPopover] = useState(false);
 
     return (
         <>
-            <Draggable nodeRef={nodeRef} grid={[window.innerWidth - 60, window.innerHeight - 10]}>
-                <div ref={nodeRef} className="flex flex-col h-full w-fit p-2 divide-transparent border-2">
-                    <button className={buttonStyles + (tool === "add" ? "bg-slate-200" : "bg-transparent")} onClick={() => activateAdd()}>
-                        <IconPlus />
-                    </button>
-                    <button className={buttonStyles + (tool === "remove" ? "bg-slate-200" : "bg-transparent")} onClick={() => {removeNode(); console.log('remove')}}>
-                        <IconX />
-                    </button>
-                    <button className={buttonStyles + (tool === "connect" ? "bg-slate-200" : "bg-transparent")} onClick={() => {activateConnect(); console.log('connect')}}>
-                        <IconLine />
-                    </button>
-                    <button className={buttonStyles + (tool === "select" ? "bg-slate-200" : "bg-transparent")} onClick={() => {selectNode(); console.log('select')}}>
-                        <IconPointer />
-                    </button>
-                    <button className={buttonStyles + (tool === "play" ? "bg-slate-200" : "bg-transparent")} onClick={() => {playNode(); console.log('play')}}>
+            <div ref={nodeRef} className="flex flex-col h-full w-fit p-2 divide-transparent border-2">
+                <button className={buttonStyles + (tool === "add" ? "bg-slate-200" : "bg-transparent")} onClick={() => activateAdd()}>
+                    <IconPlus />
+                </button>
+                <button className={buttonStyles + (tool === "remove" ? "bg-slate-200" : "bg-transparent")} onClick={() => {removeNode(); console.log('remove')}}>
+                    <IconX />
+                </button>
+                <button className={buttonStyles + (tool === "connect" ? "bg-slate-200" : "bg-transparent")} onClick={() => {activateConnect(); console.log('connect')}}>
+                    <IconLine />
+                </button>
+                <button className={buttonStyles + (tool === "select" ? "bg-slate-200" : "bg-transparent")} onClick={() => {selectNode(); console.log('select')}}>
+                    <IconPointer />
+                </button>
+                <div className="flex flex-row">
+                    {playPopover && <RunAlgorithmPopover />}
+                    <button className={buttonStyles + (tool === "play" ? "bg-slate-200" : "bg-transparent")} onClick={() => {playNode(); setPlayPopover(!playPopover); console.log('play')}}>
                         <IconPlayerPlay />
                     </button>
-                    <button className={buttonStyles + (tool === "next" ? "bg-slate-200" : "bg-transparent")} onClick={() => {nextNode(); console.log('next')}}>
-                        <IconPlayerTrackNext />
-                    </button>
                 </div>
-            </Draggable>
+                <button className={buttonStyles + (tool === "next" ? "bg-slate-200" : "bg-transparent")} onClick={() => {nextNode(); console.log('next')}}>
+                    <IconPlayerTrackNext />
+                </button>
+            </div>
         </>
     );
 }
