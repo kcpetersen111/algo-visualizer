b77130c09e1a613a2e68c7b559a77357f2e210ad
Brendan <Binary141@gmail.com>
2023-11-17 22:48:14 -0700

    Break graph into a file and get bfs implemented

--- src/app/backend/algorithms/bfs.go
+++ src/app/backend/algorithms/bfs.go
@@ -1,19 +1,65 @@
 package algorithms
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type BFS struct {
 	CurrNode int
 	G        Graph
+	queue    []int
 }
 
 func (b *BFS) NextNode() int {
-	return 0
+	return b.CurrNode
 }
 
-func (b *BFS) Search() int {
+func (b *BFS) StepSearch() (int, bool, error) {
 	// Search using BFS
-	fmt.Println("In!")
-	fmt.Printf("Graph size: %d\n", b.G.size)
-	return 1
+	fmt.Printf("Queue: %+v", b.queue)
+	fmt.Printf("visited: %+v", b.G.visited)
+	if len(b.queue) == 0 {
+		return 0, true, errors.New("Queue was empty while trying to get a new child!")
+	}
+
+	// get the next node in the list
+	b.CurrNode = b.queue[0]
+
+	// make sure to mark it as visited to be sure
+	b.G.visited[b.CurrNode] = 1
+
+	// remove the element from the beginning of the list
+	b.queue = b.queue[1:]
+
+	for child := range b.GenerateChildren() {
+		// make sure we haven't been here before
+		if b.G.visited[child] == 1 {
+			continue
+		}
+
+		// Mark as visited
+		b.G.visited[child] = 1
+
+		// don't modify the queue if we are at the end
+		if b.G.AllVisited() {
+			return b.CurrNode, true, nil
+		}
+
+		// Push to the queue
+		b.queue = append(b.queue, child)
+	}
+
+	return b.CurrNode, false, nil
+}
+
+func (b *BFS) GenerateChildren() []int {
+	// looks at the current node and gets the nodes indexes that it can go to
+	ret := make([]int, 0)
+	for i := range b.G.adjacency[b.CurrNode] {
+		if b.G.adjacency[b.CurrNode][i] != -1 {
+			ret = append(ret, i)
+		}
+	}
+	return ret
 }
---
+++ src/app/backend/algorithms/graph.go
@@ -0,0 +1,51 @@
+package algorithms
+
+import "errors"
+
+type Graph struct {
+	size int
+	// adjacency graph
+	//[startNode][endNode]
+	adjacency [][]int
+	startNode int
+	goalNode  int
+	visited   []int
+}
+
+func (g *Graph) SetSize(size int) int {
+	g.size = size
+	for i := 0; i < size; i++ {
+		a := make([]int, size)
+		for j := range a {
+			a[j] = -1
+		}
+		g.adjacency = append(g.adjacency, a)
+	}
+
+	g.visited = make([]int, size)
+
+	return g.size
+}
+
+func (g *Graph) Nodes() [][]int {
+	return g.adjacency
+}
+
+func (g *Graph) AddConnection(startNode, endNode int) error {
+	if startNode >= len(g.adjacency) || endNode >= len(g.adjacency[startNode]) {
+		return errors.New("Invalid index!")
+	}
+
+	g.adjacency[startNode][endNode] = 1
+	return nil
+}
+
+func (g *Graph) AllVisited() bool {
+	// returns if all nodes have been visited
+	for i := range g.visited {
+		if g.visited[i] == 0 {
+			return false
+		}
+	}
+	return true
+}
