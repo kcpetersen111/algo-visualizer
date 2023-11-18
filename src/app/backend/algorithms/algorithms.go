753842760a6e868f6c02c7621e2a7be9d1fb5a27
Brendan <Binary141@gmail.com>
2023-11-17 20:03:22 -0700

    Add in more skeletonizing for BFS

--- src/app/backend/algorithms/algorithms.go
+++ src/app/backend/algorithms/algorithms.go
@@ -1,7 +1,15 @@
 package algorithms
 
-type search interface {
-	SetSize() int
+import "fmt"
+
+type Search interface {
+	SetSize(int) int
 	Search() int
 	NextNode() int
+	AddConnection()
+}
+
+func Test() int {
+	fmt.Println("Hello from the algo module!")
+	return 0
 }
--- src/app/backend/algorithms/bfs.go
+++ src/app/backend/algorithms/bfs.go
@@ -1,7 +1,51 @@
 package algorithms
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func main() {
-	fmt.Println("vim-go")
+type BFS struct {
+	size     int
+	currNode int
+	// adjacency graph
+	//[startNode][endNode]
+	nodes     [][]int
+	startNode int
+	goalNode  int
+}
+
+func (b *BFS) SetSize(size int) int {
+	b.size = size
+	for i := 0; i < size; i++ {
+		a := make([]int, size)
+		b.nodes = append(b.nodes, a)
+	}
+
+	return b.size
+}
+
+func (b *BFS) Search() int {
+	// Search using BFS
+	fmt.Println("In the search!")
+	return 1
+}
+
+func (b *BFS) Nodes() [][]int {
+	return b.nodes
+}
+
+func (b *BFS) NextNode() int {
+	// Return next node from Search?
+	fmt.Println("In the NextNode!")
+	return 1
+}
+
+func (b *BFS) AddConnection(startNode, endNode int) error {
+	if startNode >= len(b.nodes) || endNode >= len(b.nodes[startNode]) {
+		return errors.New("Invalid index!")
+	}
+
+	b.nodes[startNode][endNode] = 1
+	return nil
 }
