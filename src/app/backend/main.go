c031dc8aa058a8a8d8893c6eb4edae89937901ea
Brendan <Binary141@gmail.com>
2023-11-18 06:02:24 -0700

    add test stuff to main func

--- src/app/backend/main.go
+++ src/app/backend/main.go
@@ -12,20 +12,44 @@ import (
 )
 
 func main() {
-	//graph := algorithms.Graph{}
-	//size := graph.SetSize(5)
-	//fmt.Printf("before adding connection: %+v\n", graph.Nodes())
-	//err := graph.AddConnection(0, 1)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//algo := algorithms.BFS{}
-	//algo.G = graph
-	//algo.Search()
-	//fmt.Printf("after adding connection: %+v\n", graph.Nodes())
+	// graph := algorithms.Graph{}
+	// _ = graph.SetSize(5)
+	// graph.SetStartNode(0)
+	// graph.SetGoal(2)
+	// fmt.Printf("before adding connection: %+v\n", graph.Nodes())
+	// err := graph.AddConnection(0, 1)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// err = graph.AddConnection(1, 3)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// err = graph.AddConnection(1, 4)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// err = graph.AddConnection(0, 2)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// algo := algorithms.NewDFS(graph)
 
-	// ret := algorithms.Test()
-	//fmt.Printf("size: %d\n", size)
+	// fmt.Printf("after adding connection: %+v\n", graph.Nodes())
+
+	// for {
+	// 	node, done, err := algo.StepSearch()
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		return
+	// 	}
+	// 	if done {
+	// 		fmt.Printf("Found the goal node %d\n", node)
+	// 		return
+	// 	}
+	// 	fmt.Printf("On node %d\n", node)
+	// }
+
 	host := flag.String("host", "0.0.0.0", "The host to listen at")
 	port := flag.String("port", "3410", "The host to listen at")
 	flag.Parse()
@@ -49,5 +73,4 @@ func main() {
 		ReadTimeout:  15 * time.Second,
 	}
 	log.Fatal(srv.ListenAndServe())
-
 }
